44acf16fad5a5f04d11779021673e46591620ae3
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-01 16:53:39 +0530

    Add. Creation of todo functionality

---
+++ app.js
@@ -0,0 +1,23 @@
+const addTodoForm = document.querySelector('.add');
+const todoList = document.querySelector('.todos');
+
+const addTodoWithTemplate = (todo) => {
+    const todoTemplate = `
+        <li class="list-group-item text-light border-0 my-1 rounded-3 d-flex justify-content-between">
+            <span>${todo}</span>
+            <i class="bi bi-trash-fill delete"></i>
+        </li>
+        `;
+    
+    todoList.innerHTML += todoTemplate;
+};
+
+addTodoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const todo = addTodoForm.add.value.trim();
+
+    if(todo.length != 0) {
+        addTodoWithTemplate(todo);
+    }
+})
